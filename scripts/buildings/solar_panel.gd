a485196b5252acd998a3b52258dc927e0cf2f508
debugmi530-del <debugmi530@gmail.com>
2026-05-24 17:52:40 +0300

    Fix solar_panel: proper energy generation

--- scripts/buildings/solar_panel.gd
+++ scripts/buildings/solar_panel.gd
@@ -1,14 +1,11 @@
 extends StaticBody3D
 
-var energy_per_second: float = 5.0
-var is_active: bool = true
+var energy_per_second: float = 2.0
 
-func _ready():
-	energy_per_second = 5.0 * GameManager.upgrades.get("auto_solar", 1)
-
-func _process(delta):
-	if is_active:
-		GameManager.add_resource("energy", energy_per_second * delta)
-
-func interact(_player):
-	is_active = !is_active
+func _process(delta: float):
+	var cap  = 500.0 + GameManager.upgrades.get("base_storage", 1) * 200.0
+	var rate = energy_per_second * delta
+	GameManager.resources["energy"] = min(
+		GameManager.resources.get("energy", 0.0) + rate, cap
+	)
+	GameManager.resources_changed.emit()
