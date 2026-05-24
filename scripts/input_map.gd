f82aeae285620ae797eacee987a0d7989265b438
debugmi530-del <debugmi530@gmail.com>
2026-05-24 17:32:35 +0300

    Add scripts/input_map.gd

---
+++ scripts/input_map.gd
@@ -0,0 +1,12 @@
+# This file documents the input map setup needed for Stellar Base
+# Add these actions in Project > Project Settings > Input Map in Godot Editor
+# Or they will be configured via code at runtime
+
+# move_forward - W key / Left stick up
+# move_back    - S key / Left stick down
+# move_left    - A key / Left stick left
+# move_right   - D key / Left stick right
+# jump         - Space / Cross button
+# interact     - E key / Square button
+# sprint       - Shift / L3
+# ui_cancel    - Escape / Circle button
