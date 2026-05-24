33038dd447acb32a976628e89cf997557d397e7d
debugmi530-del <debugmi530@gmail.com>
2026-05-24 18:07:16 +0300

    Add graphics quality selector: Low/Medium/High for budget Android

--- scripts/settings_screen.gd
+++ scripts/settings_screen.gd
@@ -1,37 +1,64 @@
 extends Control
 
-@onready var music_slider: HSlider = $Panel/VBoxContainer/MusicSlider
-@onready var sfx_slider: HSlider = $Panel/VBoxContainer/SFXSlider
-@onready var sensitivity_slider: HSlider = $Panel/VBoxContainer/SensSlider
-@onready var invert_y_check: CheckButton = $Panel/VBoxContainer/InvertYCheck
-@onready var quality_option: OptionButton = $Panel/VBoxContainer/QualityOption
-@onready var close_btn: Button = $Panel/CloseBtn
-@onready var apply_btn: Button = $Panel/ApplyBtn
+@onready var close_btn:       Button      = $Panel/CloseBtn
+@onready var music_slider:    HSlider     = $Panel/VBox/MusicSlider
+@onready var sfx_slider:      HSlider     = $Panel/VBox/SFXSlider
+@onready var sens_slider:     HSlider     = $Panel/VBox/SensSlider
+@onready var invert_y:        CheckButton = $Panel/VBox/InvertY
+@onready var quality_low:     Button      = $Panel/VBox/QualityRow/LowBtn
+@onready var quality_medium:  Button      = $Panel/VBox/QualityRow/MedBtn
+@onready var quality_high:    Button      = $Panel/VBox/QualityRow/HighBtn
+@onready var quality_label:   Label       = $Panel/VBox/QualityRow/CurrentLabel
+@onready var fps_label:       Label       = $Panel/VBox/FPSLabel
 
 func _ready():
-	close_btn.pressed.connect(func(): visible = false)
-	apply_btn.pressed.connect(_apply_settings)
 	visible = false
+	if close_btn:    close_btn.pressed.connect(func(): visible = false)
+	if music_slider: music_slider.value = GameManager.settings.get("music_volume", 0.8)
+	if sfx_slider:   sfx_slider.value   = GameManager.settings.get("sfx_volume",   1.0)
+	if sens_slider:  sens_slider.value  = GameManager.settings.get("sensitivity",  0.3)
+	if invert_y:     invert_y.button_pressed = GameManager.settings.get("invert_y", false)
 
-func _on_visibility_changed():
-	if visible:
-		_load_settings()
+	if music_slider: music_slider.value_changed.connect(_on_music)
+	if sfx_slider:   sfx_slider.value_changed.connect(_on_sfx)
+	if sens_slider:  sens_slider.value_changed.connect(_on_sens)
+	if invert_y:     invert_y.toggled.connect(_on_invert)
 
-func _load_settings():
-	var s = GameManager.settings
-	music_slider.value = s.get("music_volume", 0.8)
-	sfx_slider.value = s.get("sfx_volume", 1.0)
-	sensitivity_slider.value = s.get("sensitivity", 0.3)
-	invert_y_check.button_pressed = s.get("invert_y", false)
-	quality_option.selected = s.get("graphics_quality", 1)
+	if quality_low:    quality_low.pressed.connect(func():    _set_quality(0))
+	if quality_medium: quality_medium.pressed.connect(func(): _set_quality(1))
+	if quality_high:   quality_high.pressed.connect(func():   _set_quality(2))
 
-func _apply_settings():
-	GameManager.settings["music_volume"] = music_slider.value
-	GameManager.settings["sfx_volume"] = sfx_slider.value
-	GameManager.settings["sensitivity"] = sensitivity_slider.value
-	GameManager.settings["invert_y"] = invert_y_check.button_pressed
-	GameManager.settings["graphics_quality"] = quality_option.selected
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), linear_to_db(music_slider.value))
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("SFX"), linear_to_db(sfx_slider.value))
+	PerformanceManager.quality_changed.connect(_update_quality_ui)
+	_update_quality_ui(PerformanceManager.current_quality)
+
+func _process(_delta):
+	if fps_label and visible:
+		fps_label.text = "FPS: " + str(Engine.get_frames_per_second())
+
+func _set_quality(level: int):
+	PerformanceManager.set_quality(level)
 	GameManager.save_game()
-	visible = false
+
+func _update_quality_ui(level: int):
+	if quality_label:
+		var labels = ["Низкое (рекомендуется)", "Среднее", "Высокое"]
+		quality_label.text = labels[level]
+
+	var colors = [Color.GREEN, Color.YELLOW, Color.ORANGE_RED]
+	if quality_low:    quality_low.modulate    = colors[0] if level == 0 else Color.WHITE
+	if quality_medium: quality_medium.modulate = colors[1] if level == 1 else Color.WHITE
+	if quality_high:   quality_high.modulate   = colors[2] if level == 2 else Color.WHITE
+
+func _on_music(v: float):
+	GameManager.settings["music_volume"] = v
+	AudioManager._apply_volumes()
+
+func _on_sfx(v: float):
+	GameManager.settings["sfx_volume"] = v
+	AudioManager._apply_volumes()
+
+func _on_sens(v: float):
+	GameManager.settings["sensitivity"] = v
+
+func _on_invert(v: bool):
+	GameManager.settings["invert_y"] = v
